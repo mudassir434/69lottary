301fb73f34c5727981b5c1417bec217a7a3bc255
Muhammad Salman <mohammadsalman3110@gmail.com>
2024-01-15 12:30:43 +0500

    deploy code

---
+++ src/config/connectDB.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql2/promise');
+
+const connection = mysql.createPool({
+    host: 'localhost',
+    user: 'demo',
+    password: 'k6hw733KbiLb43zM',
+    database: 'demo'
+});
+
+// const connection = mysql.createPool({
+//     host: 'localhost',
+//     user: 'admin',
+//     password: 'Dph51mO5qkS8U1k',
+//     database: '92lottery'
+// });
+
+export default connection;
