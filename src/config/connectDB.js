bc9683b30449d2a8ebfcd6dd6a3da162c7bac6d8
mudassir434 <147974641+mudassir434@users.noreply.github.com>
2024-01-15 16:06:49 +0500

    Update connectDB.js

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -2,16 +2,16 @@ const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
     host: 'http://malikmysql.67d13c0402.onlitegix.com:35715',
-    user: 'demo',
-    password: 'k6hw733KbiLb43zM',
-    database: 'demo'
+    user: 'grabdeal_97',
+    password: 'grabdeal_97',
+    database: 'grabdeal_97'
 });
 
 // const connection = mysql.createPool({
-//     host: 'localhost',
-//     user: 'admin',
-//     password: 'Dph51mO5qkS8U1k',
-//     database: '92lottery'
+//     host: 'http://malikmysql.67d13c0402.onlitegix.com:35715',
+//     user: 'grabdeal_97',
+//     password: 'grabdeal_97',
+//     database: 'grabdeal_97'
 // });
 
 export default connection;
